c125a2c257ed828af671429c4dd08ea1de5d7c78
Simon Menke <simon.menke@gmail.com>
2010-05-05 18:16:17 +0200

    Speading up the test suite

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,34 +2,44 @@ require 'rubygems'
 require 'shoulda'
 require 'transitions'
 
+
+tmp = File.expand_path('../../tmp', __FILE__)
+FileUtils.mkdir_p(tmp)
+FileUtils.rm_f(tmp)
+
+ActiveRecord::Base.establish_connection({
+  :adapter => 'sqlite3',
+  :database => tmp+'/test.db'
+})
+
+ActiveRecord::Schema.define do
+  create_table "posts", :force => true do |t|
+    t.column "title", :string
+    t.column "body",  :text
+  end
+
+  create_table "pages", :force => true do |t|
+    t.column "title", :string
+    t.column "body",  :string
+    t.column "image_id", :integer
+  end
+
+  create_table "images", :force => true do |t|
+    t.column "caption", :text
+    t.column "filename", :string
+  end
+end
+
 class ActiveSupport::TestCase
   def reset_connection
     tmp = File.expand_path('../../tmp', __FILE__)
-    FileUtils.mkdir_p(tmp)
-    FileUtils.rm_f(tmp+'/test.db')
-
+    FileUtils.rm_f(tmp+'/test_real.db')
+    FileUtils.cp(tmp+'/test.db', tmp+'/test_real.db')
+    
     ActiveRecord::Base.establish_connection({
       :adapter => 'sqlite3',
-      :database => tmp+'/test.db'
+      :database => tmp+'/test_real.db'
     })
-
-    ActiveRecord::Schema.define do
-      create_table "posts", :force => true do |t|
-        t.column "title", :string
-        t.column "body",  :text
-      end
-
-      create_table "pages", :force => true do |t|
-        t.column "title", :string
-        t.column "body",  :string
-        t.column "image_id", :integer
-      end
-
-      create_table "images", :force => true do |t|
-        t.column "caption", :text
-        t.column "filename", :string
-      end
-    end
   end
 end
 
